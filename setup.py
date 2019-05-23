b6c7098ec572fe3a6592929d59bd984671c5666a
Alison <awon8465@uni.sydney.edu.au>
2019-05-24 03:37:46 +1000

    changed name of package to gaga

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
-
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='genalgo',  
-     version='0.1',
-     scripts=['genalgo'] ,
+     name='gaga',
+     version='0.1.0',
      author="Alison Wong",
      author_email="a.wong@sydney.edu.au",
      description="A genetic algorithm",
      long_description=long_description,
    long_description_content_type="text/markdown",
-     url="",
+     url="https://github.com/awon8465/GenAlg",
      packages=setuptools.find_packages(),
      classifiers=[
          "Development Status :: 4 - Beta",
@@ -22,9 +20,6 @@ setuptools.setup(
     install_requires=[
         'numpy',
         'matplotlib',
-        'bz2',
-        'pickle',
-        'sys',
-        'os'
+        'bz2file',
     ]
  )
